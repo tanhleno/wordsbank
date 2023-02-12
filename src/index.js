8284e3bb60553772f11fd150413d662d25630205
Tanhleno Teixeira de Sousa <tts.tanhleno@gmail.com>
2023-02-11 22:11:18 -0300

    Create global style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './app';
 
+import './style.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
